d858ed3b7c0939b70c442fb4046e6cd34992118b
André Voronhuk <andrevoronhuk@gmail.com>
2021-11-22 12:31:20 -0300

    utilizando novos componentes T

--- TField.php
+++ TField.php
@@ -1,16 +1,16 @@
 <?php
-
+include_once 'TElement.php';
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+* To change this license header, choose License Headers in Project Properties.
+* To change this template file, choose Tools | Templates
+* and open the template in the editor.
+*/
 
 /**
- * Description of TField
- *
- * @author marca
- */
+* Description of TField
+*
+* @author marca
+*/
 abstract class TField {
 
     protected $name;
@@ -20,96 +20,99 @@ abstract class TField {
     protected $tag;
 
     /**
-     * método construtor
-     * instancia um campo do formulario
-     * Qparam S$name = nome do campo
-     */
-    public function __construct($name) {
-// define algumas características iniciais
-        self::setEditable(true);
-        self::setName($name);
-        self::setSize(200);
-// Instancia um estilo CSS chamado tfield
-// que será utilizado pelos campos do formulário
-//! cria uma tag HTML do tipo <input>
-        $this->tag = new TElement('input');
-//$this->tag->class = 'tfield'; // classe CSS
+    * método construtor
+    * instancia um campo do formulario
+    * Qparam S$name = nome do campo
+    */
+
+    public function __construct( $name ) {
+        // define algumas características iniciais
+        self::setEditable( true );
+        // self::setName( $name );
+        self::setSize( 200 );
+        // Instancia um estilo CSS chamado tfield
+        // que será utilizado pelos campos do formulário
+        //! cria uma tag HTML do tipo <input>
+        $this->tag = new TElement( 'input' );
+        //$this->tag->class = 'tfield';
+        // classe CSS
         /*
-         * método setName()
-         * define o nome do widget
-         * Qparam name = nome do widget
-         */
+        * método setName()
+        * define o nome do widget
+        * Qparam name = nome do widget
+        */
     }
 
-    public function __setName($name) {
+    public function __setName( $name ) {
         $this->name = $name;
     }
 
     /*
-     * método getName()
-     * retorna o nome do widget
-     */
+    * método getName()
+    * retorna o nome do widget
+    */
 
     public function getName() {
         return $this->name;
     }
 
     /*
-     * método setValue()
-     * define o valor de um campo
-     * Qparam Svalue = valor do campo
-     */
+    * método setValue()
+    * define o valor de um campo
+    * Qparam Svalue = valor do campo
+    */
 
-    public function setValue($value) {
+    public function setValue( $value ) {
         $this->value = $value;
     }
 
     /*
-     * método getValue()
-     */
+    * método getValue()
+    */
 
     public function getValue() {
-//retorna o valor de um campo
+        //retorna o valor de um campo
         return $this->value;
     }
 
     /**
-     * método setEditable()
-     * define se o campo poderá ser editado
-     * @Gparam Seditable = TRUE ou FALSE
-     */
-    public function setEditable($editable) {
+    * método setEditable()
+    * define se o campo poderá ser editado
+    * @Gparam Seditable = TRUE ou FALSE
+    */
+
+    public function setEditable( $editable ) {
         $this->editable = $editable;
     }
 
     /*
-     * método getEditable()
-     * retorna o valor da propriedade Seditable
-     */
+    * método getEditable()
+    * retorna o valor da propriedade Seditable
+    */
 
     public function getEditable() {
         return $this->editable;
     }
 
     /*
-     * método setProperty O
-     * define uma propriedade para o campo
-     * @param Sname = nome da propriedade
-     * @param $valor = valor da propriedade
-     */
+    * método setProperty O
+    * define uma propriedade para o campo
+    * @param Sname = nome da propriedade
+    * @param $valor = valor da propriedade
+    */
 
-    public function setProperty($name, $value) {
-// define uma propriedade de $this->tag
+    public function setProperty( $name, $value ) {
+        // define uma propriedade de $this->tag
         $this->tag->$name = $value;
     }
 
-/*
-     * método setSize()
-     * define a largura do widget
-     * @param S$size = tamanho em pixels
-     */
+    /*
+    * método setSize()
+    * define a largura do widget
+    * @param S$size = tamanho em pixels
+    */
 
-    public function setSize($size) {
+    public function setSize( $size ) {
         $this->size = $size;
     }
 
--- index.php
+++ index.php
@@ -1,89 +1,96 @@
 <?php
 include_once 'TStyle.php';
+include_once 'TParagraph.php';
+include_once 'TElement.php';
+include_once 'TEntry.php';
 
-
-
-
-$estilo1 = new TStyle('texto-azul');
+$estilo1 = new TStyle( 'texto-azul' );
 $estilo1->color = 'rgb(0,100,255)';
 $estilo1->show();
 
-$e2 = new TStyle('texto-maior');
+$e2 = new TStyle( 'texto-maior' );
 $e2->color = 'rgb(0,255,100)';
-$e2->font_size = "29px";
+$e2->font_size = '29px';
 $e2->show();
 
-$e3 = new TStyle('ul');
+$e3 = new TStyle( 'ul' );
 $e3->color = 'rgb(200,200,250)';
-$e3->list_style_type = "square";
-$e3->font_variant = "small-caps";
-$e2->font_size = "28px";
-
+$e3->list_style_type = 'square';
+$e3->font_variant = 'small-caps';
+$e2->font_size = '28px';
 $e3->show();
 
-$e4 = new TStyle('code');
-$e4->font_family = "Consolas";
-$e4->background_color = "#555";
-$e4->color = "#0D0";
+$e4 = new TStyle( 'code' );
+$e4->font_family = 'Consolas';
+$e4->background_color = '#555';
+$e4->border_radius = '10px';
+$e4->style = 'padding:1rem;';
+$e4->color = '#0D0';
 $e4->show();
 
+$e5 = new TParagraph( 'Eu sou um TParagraph! ' );
 
+$e5->style = 'color:#0D0';
+
+$e6 = new TEntry( 'teste' );
+$e6->placeholder = 'Eu Sou um INPUT!';
+
+$contai = new TElement( 'div' );
+
+$contai->add( $e5 );
+$contai->add( $e6 );
+$contai->style = 'margin:1.5rem;';
+$contai->show();
 
 ?>
 <style>
-    body,
-    html {
-        background-color: #222223;
-        margin: 0;
-        padding: 0;
-    }
+body,
+html {
+    background-color: #222223;
+    margin: 0;
+    padding: 0;
+}
 
-    .container {
-        margin: 1rem;
-        padding: 1rem;
-        border: 1px solid #555;
-        background-color: #333;
-        border-radius: 8px;
-        font-family: -apple-system, BlinkMacSystemFont,
-            'Segoe UI', 'Open Sans', 'Helvetica Neue', sans-serif;
+.container {
+    margin: 1rem;
+    padding: 1rem;
+    border: 1px solid #555;
+    background-color: #333;
+    border-radius: 8px;
+    font-family: -apple-system, BlinkMacSystemFont,
+    'Segoe UI', 'Open Sans', 'Helvetica Neue', sans-serif;
 
-    }
+}
 </style>
-<div class="container">
-    <p class="texto-azul">
+<div class = 'container'>
+<p class = 'texto-azul'>
 
-        Eu sou um texto simples em Azul!
-    </p>
-    <p class="texto-maior">
-        Sou um texto maior!
-    </p>
+Eu sou um texto simples em Azul!
+</p>
+<p class = 'texto-maior'>
+Sou um texto maior!
+</p>
 
-    <div class="ul">
-        <li> Item 1 da lista</li>
-        <li> Item 2 da lista</li>
-        <li> Item 3 da lista</li>
-    </div>
-    <br><br>
+<div class = 'ul'>
+<li> Item 1 da lista</li>
+<li> Item 2 da lista</li>
+<li> Item 3 da lista</li>
+</div>
+<br><br>
 
-    <div class="code">
-        <pre>
+<div class = 'code'>
+<pre>
 
-            eu sou um código em um campo code!
-
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-            Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
-             when an unknown printer took a galley of type and scrambled it to make a type 
-             specimen book. It has survived not only five centuries, but also the leap into
-              electronic typesetting, remaining essentially unchanged. It was popularised in
-               the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
-                and more recently with desktop publishing software Aldus including versions of
-                 Lorem Ipsum.
-        </pre>
-    </div>
-
-
-
-
-
+' eu sou um código em um campo code!
+    Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+when an unknown printer took a galley of type and scrambled it to make a type
+specimen book. It has survived not only five centuries, but also the leap into
+electronic typesetting, remaining essentially unchanged. It was popularised in
+the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+and more recently with desktop publishing software Aldus including versions of
+Lorem Ipsum.'
+</pre>
+</div>
 
 </div>
